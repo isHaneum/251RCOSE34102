4d2b7539930895cc9b44b10b5d2a455f5b43155b
isHaneum <82310007+isHaneum@users.noreply.github.com>
2025-05-28 14:30:31 +0900

    Update waiting_queue.h

--- waiting_queue.h
+++ waiting_queue.h
@@ -4,7 +4,7 @@
 
 #include "process.h"
 
-// I/O 대기 큐 노드
+// I/O 대기 큐 node
 typedef struct WNode {
     Process* p;
     struct WNode* next;
@@ -40,11 +40,11 @@ void waiting_dequeue(WaitingQueue* wq);
 #include "waiting_queue.h"
 
 // FCFS 스케줄링 실행
-// max_time: 시뮬레이션 최대 시간(ms)
+// max_time
 void schedule_fcfs(Process procs[], int n, int max_time);
 
 // Round Robin 스케줄링 실행
-// time_quantum: 타임 퀀텀(ms)
+// time_quantum
 void schedule_rr(Process procs[], int n, int time_quantum, int max_time);
 
 #endif // SCHEDULER_H
