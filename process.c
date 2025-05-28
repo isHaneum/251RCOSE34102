bba7236e24c5363ad5eccdec3bab37af565bed26
isHaneum <82310007+isHaneum@users.noreply.github.com>
2025-05-28 10:17:16 +0900

    process.c

--- process.c
+++ process.c
@@ -16,17 +16,17 @@ Process* create_random_process(const char* id, int pid) {
     strncpy(p->id, id, ID_LEN-1);
     p->id[ID_LEN-1] = '\0';
 
-    p->arrival_time     = rand() % 6;         // 도착 시간 랜덤 0~15
-    p->burst_time       = rand() % 10 + 2;      // burst 시간 랜덤 2~11
+    p->arrival_time     = rand() % 11;         // 도착 시간 랜덤 0~10
+    p->burst_time       = rand() % 9 + 2;      // burst 시간 랜덤 2~10
     p->io_burst_time    = rand() % 4 + 2;      // io burst 시간 랜덤 2~5
     p->remaining_time   = p->burst_time;       // burst 후 남은 bursttime
     p->priority         = rand() % 8;          // 우선 순위 랜덤 0~7 
-    p->executed_time    = 0;                   // 실행된 순간부터 시간, 재실행시 초기화 1초 이상 실행됐는지 알기 위해해
+    p->executed_time    = 0;                   // 실행된 순간부터 시간, 재실행시 초기화 1초 이상 실행됐는지 알기 위해
     p->total_executed   = 0;                   // 전부 얼마나 실행 됐는지?
     p->io_complete_time = 0;                   // io 완료 시간
     p->waiting_time     = 0;                   // waiting time == completed_time - arrival_time - burst_time
     p->turnaround_time  = 0;                   // turnaround time == wating_time + burst_time
-    p -> completed_time = 0;                   // process 완료 시간(turnaound 계산용용)
+    p -> completed_time = 0;                   // process 완료 시간(turnaound 계산용)
 
     return p;
 }
@@ -42,7 +42,7 @@ void create_processes(Process procs[], int n) {
     }
 }
 
-//process 필수요소 외 나머지 다 초기화
+//process 고정요소 외 나머지 다 초기화 (arriv, burst, io, priority)
 void init_processes(Process procs[], int n) {
     for (int i = 0; i < n; i++) {
         procs[i].remaining_time   = procs[i].burst_time;
