10d39137e1c0b269ff60826ff34fa61e48582348
isHaneum <82310007+isHaneum@users.noreply.github.com>
2025-06-10 10:39:46 +0900

    Update waiting_queue

--- waiting_queue.h
+++ waiting_queue.h
@@ -21,26 +21,9 @@ typedef struct {
 void init_waiting_queue(WaitingQueue* wq);
 // 대기 큐에 삽입
 void waiting_enqueue(WaitingQueue* wq, Process* p);
-// 대기 큐 맨 앞 프로세스 확인
+// waiting 큐 맨 앞 확인
 Process* waiting_peek(WaitingQueue* wq);
-// 대기 큐 맨 앞 프로세스 제거
+// waiting 큐 맨 앞 제거
 void waiting_dequeue(WaitingQueue* wq);
 
 #endif // WAITING_QUEUE_H
-
-
-// include/scheduler.h
-#ifndef SCHEDULER_H
-#define SCHEDULER_H
-
-#include "process.h"
-#include "ready_queue.h"
-#include "waiting_queue.h"
-
-// FCFS
-void schedule_fcfs(Process procs[], int n, int max_time);
-
-// Round Robin
-void schedule_rr(Process procs[], int n, int time_quantum, int max_time);
-
-#endif // SCHEDULER_H
